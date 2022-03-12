483ace9a05c39c47e257529db74e7d3b28ae582d
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-03-12 21:39:44 +0200

    imported our custom button

--- e-commerce-clothing/src/components/sign-in/sign-in.component.jsx
+++ e-commerce-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import './sign-in.styles.scss';
 
@@ -51,7 +52,7 @@ class SignIn extends React.Component {
                         required
                         />
 
-                    <input type='submit' value='submit'/>
+                    <CustomButton type='submit' value='submit'> Sign in </CustomButton>
                 </form>
             </div>
         )
