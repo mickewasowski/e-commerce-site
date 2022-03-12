3cec5d3bd8a5319ce50db00ae275663c5565d0bb
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-03-12 17:06:22 +0200

    refactored and added basic routing

--- e-commerce-clothing/src/App.js
+++ e-commerce-clothing/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+
+  return(
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/hats' component={HatsPage}></Route>
+      </Switch>
+      
     </div>
   );
 }
