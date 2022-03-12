a71fa6e46c86ca24a91f8fd62cbf1409b9f6ff42
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-03-12 18:42:48 +0200

    added route for the shop page

--- e-commerce-clothing/src/App.js
+++ e-commerce-clothing/src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => {
-
-  return(
-    <div>
-      <h1>Hats page</h1>
-    </div>
-  )
-}
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={HomePage}></Route>
-        <Route path='/hats' component={HatsPage}></Route>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
       
     </div>
