a82be0d7315acdb968927f2186075e655c55a79d
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-03-12 18:42:37 +0200

    created collection preview component

--- e-commerce-clothing/src/pages/shop/shop.component.jsx
+++ e-commerce-clothing/src/pages/shop/shop.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './shop.styles.scss';
 import SHOP_DATA from './shop.data';
 
-import CollectionPreview from '../../components/preview-collection/collection-preview.component';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 
 class ShopPage extends React.Component {
