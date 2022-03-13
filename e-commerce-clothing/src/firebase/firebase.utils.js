297bbbbec27ba1e19636b9aec9fdfb8db057607f
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-03-13 20:35:50 +0200

    created a method upon google login which creates a user document with the user details

--- e-commerce-clothing/src/firebase/firebase.utils.js
+++ e-commerce-clothing/src/firebase/firebase.utils.js
@@ -11,7 +11,32 @@ const config = {
     appId: "1:108038753747:web:32be808ccd9ad158fef326"
   };
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get(); // returns exists property - boolean
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+
+    return userRef;
+  }
+}
+
+firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
