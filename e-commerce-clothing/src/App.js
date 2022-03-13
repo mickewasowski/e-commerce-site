ed473dbbc44d66b8a6633d0a7cb117409722a8d6
Rosen Yorgov <yorgov.h.r@gmail.com>
2022-03-13 16:53:01 +0200

    implemented sign in and sign out buttons
    also added styling

--- e-commerce-clothing/src/App.js
+++ e-commerce-clothing/src/App.js
@@ -23,8 +23,6 @@ class App extends React.Component {
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user});
-
-      console.log(user);
     });
   }
 
@@ -35,7 +33,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser}/>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
